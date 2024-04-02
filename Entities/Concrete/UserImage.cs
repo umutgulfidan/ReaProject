62c4a83f8e34c74c91ab81039d87f5300c3ccd01
Utku Genç <Utku-Genc>
2024-04-02 18:27:02 +0300

    Database Model

---
+++ Entities/Concrete/UserImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class UserImage
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; } 
+        public string ImagePath { get; set; }
+        public DateTime Date{ get; set; }
+        public bool? Status { get; set; }
+    }
+}
