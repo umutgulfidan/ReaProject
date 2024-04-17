c47c638e62aa74332cf5211e1b069aabd6dfc033
Ahmet EFE TOSUN <ahefto@hotmail.com>
2024-04-17 16:42:44 +0300

    Added DistrictValidator,CityValidator

---
+++ Business/ValidationRules/FluentValidation/CityValidator.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CityValidator : AbstractValidator<City>
+    {
+        public CityValidator()
+        {
+            RuleFor(c => c.CityName).NotEmpty();
+            RuleFor(c=>c.CityName).MaximumLength(50);
+            RuleFor(c=>c.CityName).MinimumLength(2);
+        }
+    }
+}
---
+++ Business/ValidationRules/FluentValidation/DistrictValidator.cs
@@ -0,0 +1,22 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class DistrictValidator : AbstractValidator<District>
+    {
+        public DistrictValidator()
+        {
+            RuleFor(d=>d.DistrictName).NotEmpty();
+            RuleFor(d=>d.CityId).NotEmpty();
+            RuleFor(d=>d.DistrictName).MinimumLength(1);
+            RuleFor(d => d.DistrictName).MaximumLength(50);
+            RuleFor(d=>d.CityId).GreaterThan(0);
+        }
+    }
+}
