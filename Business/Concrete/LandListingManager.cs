7f609e75cf5ac3d5f7cceee529cfede9b6b73748
Ahmet EFE TOSUN <ahefto@hotmail.com>
2024-04-18 20:58:27 +0300

    Added WebAPI,Business,DataAccess layers for new LandListing entity

---
+++ Business/Concrete/LandListingManager.cs
@@ -0,0 +1,54 @@
+﻿using Business.Abstract;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class LandListingManager : ILandListingService
+    {
+        ILandListingDal _landListingDal;
+
+        public LandListingManager(ILandListingDal landListingDal)
+        {
+           _landListingDal = landListingDal;
+        }
+        [ValidationAspect(typeof(LandListingValidator))]
+        public IResult Add(LandListing landListing)
+        {
+            _landListingDal.Add(landListing);
+            return new SuccessResult();
+        }
+
+        public IResult Delete(LandListing landListing)
+        {
+            _landListingDal.Delete(landListing);
+            return new SuccessResult();
+        }
+
+        public IDataResult<List<LandListing>> GetAll()
+        {
+           
+            return new SuccessDataResult<List<LandListing>>(_landListingDal.GetAll());
+        }
+
+        public IDataResult<LandListing> GetById(int id)
+        {
+            return new SuccessDataResult<LandListing>(_landListingDal.Get(ll=>ll.Id==id));
+        }
+        [ValidationAspect(typeof (LandListingValidator))]
+        public IResult Update(LandListing landListing)
+        {
+            _landListingDal.Update(landListing);
+            return new SuccessResult();
+        }
+    }
+}
---
+++ WebAPI/Controllers/LandListingController.cs
@@ -0,0 +1,79 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LandListingController : ControllerBase
+    {
+        ILandListingService _landListingService;
+
+        public LandListingController(ILandListingService landListingService)
+        {
+            _landListingService = landListingService;
+        }
+
+        [HttpGet("getall")]
+
+        public IActionResult GetAll()
+        {
+            var result = _landListingService.GetAll();
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpGet("getbyid")]
+
+        public IActionResult GetById(int id)
+        {
+            var result = _landListingService.GetById(id);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("update")]
+
+        public IActionResult Update(LandListing landListing)
+        {
+            var result = _landListingService.Update(landListing);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("add")]
+
+        public IActionResult Add(LandListing landListing)
+        {
+            var result = _landListingService.Add(landListing);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+
+        [HttpPost("delete")]
+
+        public IActionResult Delete(LandListing landListing)
+        {
+            var result = _landListingService.Delete(landListing);
+            if (result.IsSuccess)
+            {
+                return Ok(result);
+            }
+            return BadRequest();
+        }
+    }
+}
